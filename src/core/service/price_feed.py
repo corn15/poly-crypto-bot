cfa280df5be7f9dd40673ca7be940f1c086c46f7
corn <tp6m4aup3@gmail.com>
2025-09-07 20:34:46 +0800

    get past 3h candles

--- src/core/service/price_feed.py
+++ src/core/service/price_feed.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 from src.core.interface.exchange import IExchange
 from src.core.model.price import Candle
@@ -13,12 +13,15 @@ class PriceFeedService:
 
         self.price: Dict[str, float] = {}
         self.candles: Dict[str, Candle] = {}
+        self.past_3h_5m_candles: Dict[str, List[Candle]] = {}
 
     def update(self):
         for asset in self.price_feed_provider.get_assets():
             self.price[asset] = self.price_feed_provider.get_current_price(asset)
             self.candles[asset] = self.price_feed_provider.get_current_candle(asset)
-
+            self.past_3h_5m_candles[asset] = self.price_feed_provider.get_historical_candles(
+                asset, 36
+            )
         print(f"price:{self.price}")
 
     def get_price(self, asset: str) -> float:
@@ -26,3 +29,6 @@ class PriceFeedService:
 
     def get_candle(self, asset: str) -> Candle:
         return self.candles[asset]
+
+    def get_past_3h_5m_candles(self, asset: str) -> List[Candle]:
+        return self.past_3h_5m_candles[asset]
