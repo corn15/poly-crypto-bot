c297de143363947cba2fc1171a215ae6f96c699d
corn <tp6m4aup3@gmail.com>
2025-09-12 08:59:37 +0800

    check this commit, need to revise back to 3h_5m_cahndle

--- src/core/service/price_feed.py
+++ src/core/service/price_feed.py
@@ -13,14 +13,14 @@ class PriceFeedService:
 
         self.price: Dict[str, float] = {}
         self.candles: Dict[str, Candle] = {}
-        self.past_3h_5m_candles: Dict[str, List[Candle]] = {}
+        self.past_4h_5m_candles: Dict[str, List[Candle]] = {}
 
     def update(self):
         for asset in self.price_feed_provider.get_assets():
             self.price[asset] = self.price_feed_provider.get_current_price(asset)
             self.candles[asset] = self.price_feed_provider.get_current_candle(asset)
-            self.past_3h_5m_candles[asset] = self.price_feed_provider.get_historical_candles(
-                asset, 36
+            self.past_4h_5m_candles[asset] = self.price_feed_provider.get_historical_candles(
+                asset, 48
             )
         print(f"price:{self.price}")
 
@@ -30,5 +30,5 @@ class PriceFeedService:
     def get_candle(self, asset: str) -> Candle:
         return self.candles[asset]
 
-    def get_past_3h_5m_candles(self, asset: str) -> List[Candle]:
-        return self.past_3h_5m_candles[asset]
+    def get_past_4h_5m_candles(self, asset: str) -> List[Candle]:
+        return self.past_4h_5m_candles[asset]
